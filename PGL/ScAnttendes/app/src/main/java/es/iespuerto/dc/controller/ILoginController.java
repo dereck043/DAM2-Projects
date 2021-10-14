a94620ca79705bd7accbf4c875c035ac018a8b88
dereck <derecinho@gmail.com>
2021-10-14 17:39:06 +0100

    app

---
+++ PGL/ScAnttendes/app/src/main/java/es/iespuerto/dc/controller/ILoginController.java
@@ -0,0 +1,5 @@
+package es.iespuerto.dc.controller;
+
+public interface ILoginController {
+    void OnLogin(String email,String Password);
+}
---
+++ PGL/ScAnttendes/app/src/main/java/es/iespuerto/dc/model/Alumno.java
@@ -0,0 +1,5 @@
+package es.iespuerto.dc.model;
+
+public class Alumno {
+    private String nombre, apellido;
+}
