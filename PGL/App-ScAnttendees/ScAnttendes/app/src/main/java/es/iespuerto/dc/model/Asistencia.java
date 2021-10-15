a059c2ffb3e7385d8f342151d525d41dcd9c7b2e
dereck043 <derecinho@gmail.com>
2021-10-15 11:54:36 +0100

    app

---
+++ PGL/App-ScAnttendees/ScAnttendes/app/src/main/java/es/iespuerto/dc/model/Asistencia.java
@@ -0,0 +1,10 @@
+package es.iespuerto.dc.model;
+
+import java.util.Date;
+
+public class Asistencia {
+    private int idAsistencia, idColaborador, idAlumno;
+    private Date fecha;
+    private String tipoAsistencia;
+
+}
