1e4a387d2ba42af1e78885b897086d0d69c158ce
dereck043 <derecinho@gmail.com>
2021-10-17 13:07:34 +0100

    updates

--- PGL/App-ScAnttendees/ScAnttendes/app/src/main/java/es/iespuerto/dc/controller/ILoginController.java
+++
@@ -1,6 +0,0 @@
-package es.iespuerto.dc.controller;
-
-public interface ILoginController {
-    void OnLogin(String email,String clave);
-    int isValid(String email, String clave);
-}
--- PGL/App-ScAnttendees/ScAnttendes/app/src/main/java/es/iespuerto/dc/view/ILoginView.java
+++
@@ -1,6 +0,0 @@
-package es.iespuerto.dc.view;
-
-public interface ILoginView {
-    void OnLoginSuccess(String message);
-    void OnLoginError(String message);
-}
